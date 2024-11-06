d3b318ffad4461cd0e7da462ebffb3b6c69ab0c4
jkjorgie <jkjorgie@yahoo.com>
2024-11-05 19:01:19 -0700

    w2 learning activities

--- scripts/script.js
+++ scripts/script.js
@@ -111,10 +111,12 @@ function displayCoursesBySubject(subject) {
 
 document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('nav');
+    // const nav = document.querySelector('nav');
+    const nav = document.querySelector('.animate-me');
 
     hamburger.addEventListener('click', function () {
         nav.classList.toggle('active');
+        hamburger.classList.toggle('open');
     });
 });
 
